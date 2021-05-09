48d6727f6fdf01cb59f46fc7fd03cc2e2ed4b0c5
kanison konkla <53283017+pooooooon@users.noreply.github.com>
2021-05-09 20:26:52 +0700

    Update BedrockProtocolInfo.php
    415 - 417 is not working
    made some BaseGameVersion
    and clearcode

--- src/pocketmine/network/mcpe/protocol/BedrockProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/BedrockProtocolInfo.php
@@ -14,22 +14,51 @@ final class BedrockProtocolInfo {
     public const PROTOCOL_433 = 433;
 	public const PROTOCOL_437 = 437;
 
+	public const PROTOCOL_1_16_100 = "1.16.100";
+	public const PROTOCOL_1_16_200 = "1.16.200";
+	public const PROTOCOL_1_16_210 = "1.16.210";
+	public const PROTOCOL_1_16_220 = "1.16.220";
+	public const PROTOCOL_1_16_230 = "1.16.230";
+	public const PROtOCOL_1_17_0 = "1.17.0";
+
 	public static function translateProtocol(int $protocol) : int {
-		if (in_array($protocol, [414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424], true)) {
-			return BedrockProtocolInfo::PROTOCOL_419;
+		if (in_array($protocol, [418, 419, 420, 421, 422, 423, 424], true)) {
+			return self::PROTOCOL_419;
 		}
 		if (in_array($protocol, [425, 426, 427, 428], true)) {
-			return BedrockProtocolInfo::PROTOCOL_428;
+			return self::PROTOCOL_428;
 		}
 		if (in_array($protocol, [429, 430, 431], true)) {
-			return BedrockProtocolInfo::PROTOCOL_431;
+			return self::PROTOCOL_431;
 		}
 		if (in_array($protocol, [433, 434, 435, 437, 440], true)){
-			return BedrockProtocolInfo::PROTOCOL_433;
+			return self::PROTOCOL_433;
 		}
 		if (in_array($protocol, [437, 440], true)){
-			return BedrockProtocolInfo::PROTOCOL_437;
+			return self::PROTOCOL_437;
 		}
 		return $protocol;
 	}
+	public static function basegameversion(string $protocol){
+		if (in_array($protocol, [418, 419], true)){
+			return self::PROTOCOL_1_16_100;
+		}
+		if (in_array($protocol, [420, 421, 422], true)){
+			return self::PROTOCOL_1_16_200;
+		}
+		if (in_array($protocol, [423, 424, 425, 427, 428], true)){
+			return self::PROTOCOL_1_16_210;
+		}
+		if (in_array($protocol, [429, 430, 431], true)){
+			return self::PROTOCOL_1_16_220;
+		}
+		if (in_array($protocol, [433, 434, 435], true)) {
+			return self::PROTOCOL_1_16_230;
+		}
+		if (in_array($protocol, [437, 440], true)){
+		    return self::PROTOCOL_1_17_0;
+		}
+		return $protocol;
+	}
+	
 }
